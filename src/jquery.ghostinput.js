164c07526a0b39c4efacaeefcd17f412ffbcbc7e
Ravi Rajendra <ravi.rajendra@gmail.com>
2013-12-08 14:19:31 +0530

    Version 1.0 code added.

--- src/jquery.ghostinput.js
+++ src/jquery.ghostinput.js
@@ -2,6 +2,8 @@
  *
  *  jQuery ghostWriter Plugin for adding subdomain in forms
  *
+ *  Version: 1.0.0
+ *
  *  Author: GODPOD (Ravi Rajendra)
  *
  *  URL: https://github.com/godpod/javascript/ghostWriter
@@ -12,7 +14,8 @@
         var o = $.extend({
             ghostText: ".ghosttext",
             ghostPlaceholder: "Add subdomain",
-            ghostTextClass: "ghost-text"
+            ghostTextClass: "ghost-text",
+            ghostHiddenClass: ""
         }, options);
         return this.each(function(i, element){
             var $element = $(element);
@@ -24,13 +27,13 @@
             $element.attr("placeholder")?$element.removeAttr("placeholder"):"";
             o.ghostText = $element.attr("data-ghost-text") || o.ghostText;
             o.ghosttextspan = $("<label />").text(o.ghostPlaceholder);
-            o.ghostHider = $("<label />").css({"visibility":"hidden"});
-            o.ghostBox = $("<label />").attr("for", r).addClass(o.ghostTextClass).append(o.ghostHider).append(o.ghosttextspan);
+            o.ghostHiddenLabel = $("<label />").css({"visibility":"hidden"}).addClass(o.ghostHiddenClass);
+            o.ghostBox = $("<label />").attr("for", r).addClass(o.ghostTextClass).append(o.ghostHiddenLabel).append(o.ghosttextspan);
             $element.parent().prepend(o.ghostBox);
             $element.bind("keyup keydown keypress change",function() {
                 setTimeout(function() {
                     var t = "" == $.trim($element.val()) ? o.ghostPlaceholder: o.ghostText;
-                    o.ghostHider.text($element.val());
+                    o.ghostHiddenLabel.text($element.val());
                     o.ghosttextspan.text(t)
                 }, 0)
             });
